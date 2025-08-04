50283ec1fa27938f761da810512df8a48d45354c
Celil Eren Kalkan <celilerenkalkan@hotmail.com>
2025-08-04 15:18:17 +0300

    UI Fixed Part 2

--- Assets/Scripts/Stats System/GameManager.cs
+++ Assets/Scripts/Stats System/GameManager.cs
@@ -11,11 +11,5 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log("Flux: " + DataManager.playerStats.TotalFlux);
-        Debug.Log("Fire Power: " + DataManager.playerStats.FirePower);
-        Debug.Log(UpgradeManager.AllUpgrades[0].GetDescription());
-        Debug.Log("Level: " + UpgradeManager.AllUpgrades[0].Level);
-        DataManager.playerStats.SetTotalFlux(20);
-        UpgradeManager.Purchase(0);
     }
 }
